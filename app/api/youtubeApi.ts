2b8b02a9da34325e7cc5570cb70878b23acbecb4
Rafael Ryu <suzukifurukawarafaelryu@gmail.com>
2026-02-26 06:56:49 -0300

    fix: update YouTube playlist API endpoint to Vercel URL

--- app/api/youtubeApi.ts
+++ app/api/youtubeApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getPlaylist(link: string) {
 
-    const response = await axios.get(`http://127.0.0.1:8000/playlist/${link}`);
+    const response = await axios.get(`https://api-vibesync.vercel.app/playlist/${link}`);
     
     return response.data; // aqui sim você retorna os dados
 }
